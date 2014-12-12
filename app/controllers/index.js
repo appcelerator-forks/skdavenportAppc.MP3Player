9af31bad9c3f3c22a3d8635aa130d4a1d8c61fc7
Scott Davenport <scott@appcelerator.com>
2014-12-12 18:26:17 -0500

    Milestone 1

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,4 +2,33 @@ function doClick(e) {
     alert($.label.text);
 }
 
+function doopen(evt) {
+    
+}
+function prevdown(evt) {
+    
+}
+
+function prevup(evt) {
+    
+}
+
+function playdown(evt) {
+    
+}
+
+function playup(evt) {
+    
+}
+
+function nextdown(evt) {
+    
+}
+
+function nextup(evt) {
+    
+}
+
+
+
 $.index.open();
